92a4e274de10bb99452a82a613bc971a07cf44b5
leeminjxun <th554751@naver.com>
2026-03-04 19:14:25 +0900

    [BOJ] 1000 A+B (구현)

---
+++ 백준/구현/BOJ_1000.java
@@ -0,0 +1,16 @@
+package 백준.구현;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_1000 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        System.out.println(A+B);
+    }
+}
