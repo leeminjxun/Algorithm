17b393f196134b9926cb3b9384f447fceb8f6d95
leeminjxun <th554751@naver.com>
2026-03-15 20:50:17 +0900

    ✅ BOJ: 1874 스택 수열 (구현 / stack, Silver2)

---
+++ 백준/구현/Sliver/BOJ_1874_스택수열.java
@@ -0,0 +1,42 @@
+package 백준.구현.Sliver;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_1874_스택수열 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+
+        // 수열 저장 스택
+        Stack<Integer> seq = new Stack<>();
+
+        // 입출력 담당 스택
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for(int i = N - 1; i >= 0; i--) {
+            seq.push(arr[i]);
+        }
+
+        for(int i = 1; i < N + 1; i++) {
+            stack.push(i);
+            sb.append("+").append("\n");
+
+            while(!stack.isEmpty() && !seq.isEmpty() && seq.peek().equals(stack.peek())) {
+                seq.pop();
+                stack.pop();
+                sb.append("-").append("\n");
+            }
+        }
+
+        if(!seq.isEmpty()) System.out.println("NO");
+        else System.out.println(sb);
+    }
+}
