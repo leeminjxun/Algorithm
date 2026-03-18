70447fe164fc4f0ab66d1221b210d2564c2a7a27
leeminjxun <th554751@naver.com>
2026-03-18 15:00:08 +0900

    ✅ BOJ: 11656 접미사 배열 (구현 / 정렬, Silver4)

---
+++ 백준/구현/Sliver/BOJ_11656_접미사배열.java
@@ -0,0 +1,29 @@
+package 백준.구현.Sliver;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_11656_접미사배열 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        List<String> list = new ArrayList<>();
+
+        String s = br.readLine();
+
+        for(int i = 0; i < s.length(); i++) {
+            String suffix = "";
+            for(int j = i; j < s.length(); j++) {
+                char c = s.charAt(j);
+                suffix += c;
+            }
+            list.add(suffix);
+        }
+
+        Collections.sort(list);
+
+        for(String suffix : list) {
+            System.out.println(suffix);
+        }
+    }
+}
