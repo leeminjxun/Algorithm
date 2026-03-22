380843e03ad7fa50fdb611e8695ef808321d4efb
leeminjxun <th554751@naver.com>
2026-03-22 17:13:36 +0900

    ✅ BOJ: 1269 대칭 차집합 (HashSet, Silver4)

---
+++ 백준/Hash/Set/Silver/BOJ_1269_대칭차집합/README.md
@@ -0,0 +1,3 @@
+# ✅ BOJ: 1269 대칭 차집합 (HashSet, Silver4)
+
+# https://www.acmicpc.net/problem/1269
--- 백준/Hash/Set/Silver/BOJ_1269_대칭차집합/대칭차집합.java
+++ 백준/Hash/Set/Silver/BOJ_1269_대칭차집합/대칭차집합.java
@@ -1,9 +1,9 @@
-package 백준.Hash.Set.Silver;
+package 백준.Hash.Set.Silver.BOJ_1269_대칭차집합;
 
 import java.io.*;
 import java.util.*;
 
-public class BOJ_1269_대칭차집합 {
+public class 대칭차집합 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
