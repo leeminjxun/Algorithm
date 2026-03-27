e506b9b9bdc2008b6bd6dde8b1d33165cdaac7d9
leeminjxun <th554751@naver.com>
2026-03-27 15:46:43 +0900

    📌 BOJ: 11722 가장 긴 감소하는 부분 수열 (DP, Silver2)

---
+++ 백준/DP/Silver/BOJ_11722_가장긴감소하는부분수열/가장긴감소하는부분수열.java
@@ -0,0 +1,38 @@
+package 백준.DP.Silver.BOJ_11722_가장긴감소하는부분수열;
+
+import java.io.*;
+import java.util.*;
+
+public class 가장긴감소하는부분수열 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] seq = new int[N + 1];
+        int[] dp = new int[N + 1];
+
+        int max = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= N; i++) {
+            int value = Integer.parseInt(st.nextToken());
+            seq[i] = value;
+            for(int j = i - 1; j >= 0; j--) {
+
+                if(seq[j] > seq[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+
+            max = Math.max(dp[i], max);
+        }
+
+        if(max == 0) System.out.println(0);
+        else System.out.println(max + 1);
+
+
+
+    }
+}
