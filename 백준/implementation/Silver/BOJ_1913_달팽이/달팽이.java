ba658cd2ec913e932bc2b8112ba9733e5a7aed69
leeminjxun <th554751@naver.com>
2026-04-01 18:05:11 +0900

    ✅ BOJ: 1913 달팽이 (구현 / 시뮬레이션, Silver3)

--- 백준/implementation/Silver/BOJ_1913_달팽이/달팽이.java
+++ 백준/implementation/Silver/BOJ_1913_달팽이/달팽이.java
@@ -5,12 +5,13 @@ import java.util.*;
 
 public class 달팽이 {
     // 하, 우, 상, 좌
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[] dx = {1, 0, -1, 0};
 
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
         int target = Integer.parseInt(br.readLine());
 
@@ -19,25 +20,23 @@ public class 달팽이 {
         int x = 0;
         int y = 0;
 
-        int targetX = 0;
-        int targetY = 0;
-
-        int num = N * N;
-
         int dir = 0;
 
-        while(num > 0) {
-            arr[y][x] = num;
+        int tx = 0;
+        int ty = 0;
 
-            if (target == num) {
-                targetX = x + 1;
-                targetY = y + 1;
+        for(int i = N * N; i >= 1; i--) {
+            arr[x][y] = i;
+
+            if(i == target) {
+                tx = x;
+                ty = y;
             }
 
             int nx = x + dx[dir];
             int ny = y + dy[dir];
 
-            if(nx < 0 || ny < 0 || nx >= N || ny >= N || arr[ny][nx] != 0) {
+            if(nx < 0 || ny < 0 || nx >= N || ny >= N || arr[nx][ny] != 0) {
                 dir = (dir + 1) % 4;
                 nx = x + dx[dir];
                 ny = y + dy[dir];
@@ -45,21 +44,18 @@ public class 달팽이 {
 
             x = nx;
             y = ny;
-
-            num--;
         }
 
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                sb.append(arr[i][j]).append(" ");
+        for(int[] row : arr) {
+            for(int col : row) {
+                sb.append(col).append(" ");
             }
             sb.append("\n");
         }
 
-        System.out.print(sb);
-        System.out.println(targetY + " " + targetX);
+        sb.append(tx + 1).append(" ").append(ty + 1);
 
-
+        System.out.println(sb);
     }
 }
