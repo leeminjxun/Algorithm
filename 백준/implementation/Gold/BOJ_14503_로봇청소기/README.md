d77e3f20b119eee036d6de8f7c2d83cd93fe1b6f
leeminjxun <th554751@naver.com>
2026-04-02 16:20:38 +0900

    ✅ BOJ: 14503 로봇 청소기 (구현 / 시뮬레이션, Gold5)

--- 백준/implementation/Gold/BOJ_14503_로봇청소기/README.md
+++ 백준/implementation/Gold/BOJ_14503_로봇청소기/README.md
@@ -1,3 +1,3 @@
-# ❌ BOJ: 14503 로봇 청소기 (구현, Gold5)
+# ✅ BOJ: 14503 로봇 청소기 (구현 / 시뮬레이션, Gold5)
 
 # https://www.acmicpc.net/problem/14503
--- 백준/implementation/Gold/BOJ_14503_로봇청소기/로봇청소기.java
+++ 백준/implementation/Gold/BOJ_14503_로봇청소기/로봇청소기.java
@@ -4,10 +4,9 @@ import java.io.*;
 import java.util.*;
 
 public class 로봇청소기 {
-
-    // 북, 동, 남, 서 ()
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {-1, 0, 1, 0};
+    // 북, 동, 남, 서
+    static int[] dr = {-1, 0, 1, 0};
+    static int[] dc = {0, 1, 0, -1};
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,7 +18,6 @@ public class 로봇청소기 {
         int[][] room = new int[N][M];
 
         st = new StringTokenizer(br.readLine());
-
         int r = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
         int d = Integer.parseInt(st.nextToken());
@@ -31,65 +29,40 @@ public class 로봇청소기 {
             }
         }
 
-        // 현재 칸이 청소되지 않았을 경우, 청소 함
-
-        // 현재 칸 주변 4칸 중 청소된 구간일 경우
-        // 1. 바라보는 방향을 유지하고, 한칸 후진 하여 청소한다.
-        // 2. 바라보는 방향 뒤쪽 칸이 벽이라면 작동을 중지
-
-        // 현재 칸 주변 4칸 중 청소되지 않은 구간이 있을 경우
-        // 1. 반시계 방향 90도 회전
-        // 2. 바라보는 방향 기준으로 앞 쪽 칸이 빈 칸일 경우 전진
-        // 청소한다.
-
-        int cnt = 0;
+        int count = 0;
 
         while(true) {
-
             if(room[r][c] == 0) {
                 room[r][c] = 2;
-                cnt += 1;
+                count++;
             }
 
-            // 다음 방향 지정 로직
-            boolean found = false;
-            int nextDir = d;
+            // 주변 4 공간이 깨끗한지 check
+            boolean isClean = true;
             for(int i = 0; i < 4; i++) {
+                d =  (d + 3) % 4;
 
-                // 반시계 방향으로 현재 방향 이동
-                nextDir = (nextDir + 3) % 4;
+                int nr = r + dr[d];
+                int nc = c + dc[d];
 
-                // 다음 방향 앞 쪽 구간이 0 일 경우
-                if(room[r + dy[nextDir]][c + dx[nextDir]] == 0) {
-                    found = true;
+                if(room[nr][nc] == 0) {
+                    isClean = false;
+                    r = nr;
+                    c = nc;
                     break;
                 }
             }
 
-            // nextDir == d 일 경우는 반시계 방향 회전을 전부 진행 -> 현재 값 주변에 청소가 안된 지역은 없음
-            // -> 청소가 된 지역 or 벽이 있는 경우
-            if(!found) {
-                // 뒤로 이동항 방향 계산, 방향 d 는 현재 값 유지
-                int dirBack = (d + 2) % 4;
+            if(isClean) {
+                r -= dr[d];
+                c -= dc[d];
 
-                // 바라보는 방향 뒤 쪽 칸이 벽이라면 작동 중지
-                if(room[r + dy[dirBack]][c + dx[dirBack]] == 1) break;
-
-                // 바라보는 방향 뒤로 이동
-                r += dy[dirBack];
-                c += dx[dirBack];
-            } else {
-
-                r += dy[nextDir];
-                c += dx[nextDir];
-                d = nextDir;
+                if(r < 0 || c < 0 || r >= N || c >= M) break;
+                if(room[r][c] == 1) break;
             }
-
         }
 
-        System.out.println(cnt);
-
-
+        System.out.println(count);
     }
 }
 
