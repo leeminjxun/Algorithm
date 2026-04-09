b825755ea4f3dafb8dfcf3cdfed3fcb4ddec02d7
leeminjxun <th554751@naver.com>
2026-04-09 16:01:42 +0900

    ❌ BOJ: 1541 잃어버린 괄호 (Greedy, Silver2)

---
+++ 백준/Greedy/Silver/BOJ_1541_잃어버린괄호/잃어버린괄호.java
@@ -0,0 +1,38 @@
+package 백준.Greedy.Silver.BOJ_1541_잃어버린괄호;
+
+import java.io.*;
+import java.util.*;
+
+public class 잃어버린괄호 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] minusGroup = br.readLine().split("-");
+
+        int totalSum = 0;
+
+        for(int i = 0; i < minusGroup.length; i++) {
+            int currentSum = calculator(minusGroup[i]);
+
+            if(i == 0) {
+                totalSum += currentSum;
+            } else {
+                totalSum -= currentSum;
+            }
+        }
+
+        System.out.println(totalSum);
+    }
+
+    static int calculator(String plusGroup) {
+        int sum = 0;
+
+        String[] plus = plusGroup.split("\\+");
+
+        for(int i = 0; i < plus.length; i++) {
+            sum += Integer.parseInt(plus[i]);
+        }
+
+        return sum;
+    }
+}
