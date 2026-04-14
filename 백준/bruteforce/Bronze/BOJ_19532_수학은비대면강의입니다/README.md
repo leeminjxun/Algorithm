2c27a39938fc012b649f3b9d5442923822168081
leeminjxun <th554751@naver.com>
2026-04-14 18:47:39 +0900

    ✅ BOJ: 19532 수학은 비대면강의입니다 (완전탐색, Bronze2)

---
+++ 백준/bruteforce/Bronze/BOJ_19532_수학은비대면강의입니다/README.md
@@ -0,0 +1,5 @@
+# ✅ BOJ: 19532 수학은 비대면강의입니다 (완전탐색, Bronze2)
+
+https://www.acmicpc.net/problem/19532
+
+# 풀이 과정
---
+++ 백준/bruteforce/Bronze/BOJ_19532_수학은비대면강의입니다/수학은비대면강의입니다.java
@@ -0,0 +1,27 @@
+package 백준.bruteforce.Bronze.BOJ_19532_수학은비대면강의입니다;
+
+import java.io.*;
+import java.util.*;
+
+public class 수학은비대면강의입니다 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+
+        for(int x = -999; x <= 999; x++) {
+            for(int y = -999; y <= 999; y++) {
+                if((a * x + b * y == c) && (d * x + e * y == f)) {
+                    System.out.println(x + " " + y);
+                    return ;
+                }
+            }
+        }
+    }
+}
