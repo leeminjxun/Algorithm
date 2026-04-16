b83ca3bb322780b1c285bcf8402ba296acfdd06f
leeminjxun <th554751@naver.com>
2026-04-16 14:37:52 +0900

    ❌ BOJ: 5568 카드 놓기 (완전탐색 / 재퀴 / Set, Silver4)

---
+++ 백준/bruteforce/Silver/BOJ_5568_카드놓기/README.md
@@ -0,0 +1,5 @@
+# ❌ BOJ: 5568 카드 놓기 (완전탐색 / 재퀴 / Set, Silver4)
+
+https://www.acmicpc.net/problem/5568
+
+# 풀이 과정
---
+++ 백준/bruteforce/Silver/BOJ_5568_카드놓기/카드놓기.java
@@ -0,0 +1,48 @@
+package 백준.bruteforce.Silver.BOJ_5568_카드놓기;
+
+import java.io.*;
+import java.util.*;
+
+public class 카드놓기 {
+    static int N, K;
+
+    static String[] cards;
+    static boolean[] visited;
+
+    static Set<String> set = new HashSet<>();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        K = Integer.parseInt(br.readLine());
+
+        cards = new String[N];
+        visited = new boolean[N];
+
+        for(int i = 0; i < N; i++) {
+            cards[i] = br.readLine();
+        }
+
+        DFS(0, "");
+
+        System.out.println(set.size());
+
+    }
+
+    static void DFS(int depth, String current) {
+        if(depth == K) {
+            set.add(current);
+            return;
+        }
+
+        for(int i = 0; i < N; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                DFS(depth + 1, current + cards[i]);
+                visited[i] = false;
+            }
+        }
+    }
+
+}
