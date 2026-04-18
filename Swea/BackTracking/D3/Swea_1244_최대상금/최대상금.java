2d4dfe7fd6c73276c9eda1f2f17410d04d76a801
leeminjxun <th554751@naver.com>
2026-04-18 18:21:42 +0900

    ❌ SWEA: 1244 최대상금 (BackTracking , D3)

---
+++ Swea/BackTracking/D3/Swea_1244_최대상금/최대상금.java
@@ -0,0 +1,69 @@
+package Swea.BackTracking.D3.Swea_1244_최대상금;
+
+import java.io.*;
+import java.util.*;
+
+public class 최대상금 {
+    static int K;
+    static String max;
+
+    static Set<String>[] visited;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for(int testCase = 1; testCase <= T; testCase++) {
+            st = new StringTokenizer(br.readLine());
+
+            String num = st.nextToken();
+            K = Integer.parseInt(st.nextToken());
+
+            visited = new HashSet[K + 1];
+
+            for(int i = 0; i <= K; i++) {
+                visited[i] = new HashSet<>();
+            }
+
+            max = "0";
+
+            dfs(num, 0);
+
+            sb.append("#" + testCase + " " + max).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    static void dfs(String num, int cnt) {
+        if(cnt == K) {
+            if(max.compareTo(num) < 0) {
+                max = num;
+            }
+            return ;
+        }
+
+        if(visited[cnt].contains(num)) return;
+        visited[cnt].add(num);
+
+        char[] arr = num.toCharArray();
+
+        for(int i = 0; i < arr.length - 1; i++) {
+            for(int j = i + 1; j < arr.length; j++) {
+                change(arr, i, j);
+                dfs(new String(arr), cnt + 1);
+                change(arr, i, j);
+            }
+        }
+
+    }
+
+    static void change(char[] arr , int i, int j) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
