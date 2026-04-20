86d0a9016ea35449d6a3f211c44f477cb21ae328
leeminjxun <th554751@naver.com>
2026-04-20 16:00:44 +0900

    ❌ SWEA: 2817 부분 수열의 합 (DP , D3)

--- Swea/DP/D3/Swea_2817_부분수열의합/부분수열의합.java
+++ Swea/DP/D3/Swea_2817_부분수열의합/부분수열의합.java
@@ -1,8 +1,9 @@
-package 백준;
+package Swea.DP.D3.Swea_2817_부분수열의합;
+
 import java.io.*;
 import java.util.*;
 
-public class Test {
+public class 부분수열의합 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -16,17 +17,13 @@ public class Test {
             int N = Integer.parseInt(st.nextToken());
             int K = Integer.parseInt(st.nextToken());
 
-            int[] arr = new int[N];
             int[] dp = new int[K + 1];
 
             dp[0] = 1;
+
             st = new StringTokenizer(br.readLine());
             for(int i = 0; i < N; i++) {
-                arr[i] = Integer.parseInt(st.nextToken());
-            }
-
-            for(int i = 0; i < N; i++) {
-                int a = arr[i];
+                int a = Integer.parseInt(st.nextToken());
 
                 for(int j = K; j >= a; j--) {
                     dp[j] += dp[j - a];
