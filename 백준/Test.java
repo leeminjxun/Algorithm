47352139cfb0ed3b663dd34d971f81aae61df26e
leeminjxun <th554751@naver.com>
2026-04-28 12:29:33 +0900

    🔁 BOJ: 2422 한윤정이 이탈리아에 가서 아이스크림을 사먹는데 (완전탐색, Sliver4)

--- 백준/Test.java
+++ 백준/Test.java
@@ -11,62 +11,38 @@ public class Test {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String s = br.readLine();
-        target = new int[26];
-        for(int i = 0; i < s.length(); i++) {
-            target[s.charAt(i) - 'A']++;
-        }
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        N = Integer.parseInt(br.readLine());
+        boolean[][] badComb = new boolean[N + 1][N + 1];
 
-        price = new int[N];
-        book = new int[N][26];
-
-        StringTokenizer st;
-        for(int i = 0; i < N; i++) {
+        for(int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
 
-            price[i] = Integer.parseInt(st.nextToken());
-            String b = st.nextToken();
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            for(int j = 0; j < b.length(); j++) {
-                book[i][b.charAt(j) - 'A']++;
+            badComb[a][b] = true;
+            badComb[b][a] = true;
+        }
+
+        int ans = 0;
+        for(int i = 1; i <= N - 2; i++) {
+            for(int j = i + 1; j <= N - 1; j++) {
+                if(badComb[i][j]) continue;
+
+                for(int k = j + 1; k <= N; k++) {
+                    if(badComb[j][k] || badComb[i][j]) continue;
+
+                    ans++;
+                }
             }
+
         }
 
-        dfs(0, 0, new int[26]);
+        System.out.println(ans);
 
-        System.out.println(res == Integer.MAX_VALUE ? -1 : res);
-
-    }
-
-    static void dfs(int depth, int totalPrice, int[] current) {
-        if(totalPrice > res) return ;
-
-        if(check(current)) {
-            res = Math.min(res, totalPrice);
-            return;
-        }
-
-        if(depth == N) return ;
-
-        int[] next = current.clone();
-        for(int i = 0; i < 26; i++) {
-            next[i] += book[depth][i];
-        }
-
-        dfs(depth + 1, totalPrice + price[depth], next);
-        dfs(depth + 1, totalPrice, current);
-    }
-
-    static boolean check(int[] current) {
-        for(int i = 0; i < 26; i++) {
-            if(current[i] < target[i]) {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
