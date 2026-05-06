486729c51aff99b39cc9b5b4043c53863061fd71
leeminjxun <th554751@naver.com>
2026-05-06 21:30:47 +0900

    ❌ SWEA: 7510 상원이의 연속합 (투 포인터 , D3)

---
+++ Swea/TwoPointer/Swea_7510_상원이의연속합/상원이의연속합.java
@@ -0,0 +1,45 @@
+package Swea.TwoPointer.Swea_7510_상원이의연속합;
+
+import java.io.*;
+import java.util.*;
+
+public class 상원이의연속합 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for(int testCase = 1; testCase <= T; testCase++) {
+            int N = Integer.parseInt(br.readLine());
+            int ans = 0;
+
+            int start = 1;
+            int end = 1;
+            int sum = 1;
+
+            while(start <= N) {
+                if(sum == N) {
+                    ans++;
+                    sum -= start;
+                    start++;
+                }
+
+                else if(sum < N) {
+                    end++;
+                    sum += end;
+                }
+
+                else if(sum > N) {
+                    sum -= start;
+                    start++;
+                }
+            }
+
+            sb.append("#").append(testCase).append(" ").append(ans).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
